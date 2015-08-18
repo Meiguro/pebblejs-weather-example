154d085aff79950e2ec22379c478e3e22b421124
Meiguro <meiguro@meiguro.com>
2015-08-18 04:03:39 -0400

    Add Weather.dailyForecastUrl

--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,19 @@
+var ajax = require('ajax');
+
 var Weather = module.exports;
+
+Weather.city = 'Toronto,CA';
+
+Weather.baseUrl = 'http://api.openweathermap.org/data/2.5';
+
+Weather.dailyForecastUrl = function() {
+  var query = ajax.formify({
+    q: Weather.city,
+    cnt: 7, // days
+    units: 'metric',
+  });
+  return Weather.baseUrl + '/forecast/daily' + '?' + query;
+};
 
 Weather.dailyForecast = function(callback) {
 };
