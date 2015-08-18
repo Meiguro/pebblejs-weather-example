63ce5c2df47e8c0c118cc73758be86b8115d0769
Meiguro <meiguro@meiguro.com>
2015-08-18 03:26:34 -0400

    Add initial home menu visuals

--- src/app.js
+++ src/app.js
@@ -3,7 +3,15 @@ var UI = require('ui');
 var App = {};
 
 App.init = function() {
-  var homeMenu = new UI.Menu();
+  var homeMenu = new UI.Menu({ fullscreen: true });
+
+  var sectionId = 0;
+  homeMenu.section(sectionId, {
+    title: 'Weather',
+    items: [{
+      title: 'Loading...'
+    }],
+  });
 
   homeMenu.show();
 };
